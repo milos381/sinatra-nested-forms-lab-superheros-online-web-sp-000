ad8bf309306f64b5c4de07056a3f94cf0368d4c7
Milos Popovic <milos_popovic@outlook.com>
2020-09-23 21:10:59 +0000

    Automatically backed up by Learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,11 @@
 class Team
     attr_reader :name, :motto
-  
+    @@all = []
     def initialize(params)
       @name = params[:name]
       @motto = params[:motto]
+    end
+    def self.all
+      @@all << self
     end
   end
